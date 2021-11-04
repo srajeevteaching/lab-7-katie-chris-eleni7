0befa6274ab2112e0e5997c186d8999bcb9dc235
baseb <cmplowman@loyola.edu>
2021-11-04 12:00:59 -0400

    Push-1

---
+++ .github/Lab7.py
@@ -0,0 +1,26 @@
+#Chris, Katie, Eleni
+#Lab7
+#CS-151
+#11/4/21
+
+import random
+
+def get_number_of_rolls():
+    rolls = input("Enter the # of rolls: ")
+    if rolls.isdigit() == False:
+        print ("Invalid input")
+        get_number_of_rolls()
+
+def roll_dice(roll1, roll2, rolls):
+    count = 0
+    while count < rolls
+    roll1 = random.randint(1, 6)
+    roll2 = random.randint(1, 6)
+    sum_of_dice = roll1 + roll2
+    return sum_of_dice
+
+#def create_histogram():
+#    index_list = [2,3,4,5,6,7,8,9,10,11,12]
+
+#def print_histogram():
+#    for __ in range __ :
